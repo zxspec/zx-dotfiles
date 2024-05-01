0346bcaec49a042c74f74c7cb2ff7b76dca48451
Dmytro Iievliev <ievlevdmitriy@gmail.com>
2024-05-01 14:09:18 +0200

    Added Brew installation for MacOS

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-# check OS.
+# set ENV variables
+export DOTFILES_URL="https://raw.githubusercontent.com/zxspec/zx-dotfiles/main"
+
+# fail on unbound variables
+set -u
+
+
+# launch OS-specific setup script
 OS="$(uname)"
 if [[ "${OS}" == "Darwin" ]]
 then
-  chmod +x ./osx.sh
-  ./osx.sh
+  curl -L "$DOTFILES_URL/osx.sh" | sh
 elif [[ "${OS}" == "Linux" ]]
 then
   abort "Not ready yet."
--- osx.sh
+++ osx.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 echo "Setting up macOS..."
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
